e0ec5fecab0cc5ceff16c899d7c55b1d37ef40e4
emreseyit <h.emre.seyit@gmail.com>
2024-06-10 17:54:56 +0300

    Message embedding bugs fixed

--- main.py
+++ main.py
@@ -6,57 +6,59 @@ PROJECT_DIR = Path(__file__).resolve().parent
 
 def encode_message(msg):
     """
-    Encodes a message to base64
+    Encodes a message to binary
     :param msg: String to be encoded
     :return: Encoded message as list of 2 bits
     """
     msg_bits = ''.join(format(ord(char), '08b') for char in msg)
-    enc_msg = [int(msg_bits[i:i+2]) for i in range(0, len(msg_bits), 2)]
+    enc_msg = [int(msg_bits[i:i + 2], 2) for i in range(0, len(msg_bits), 2)]
     return enc_msg
 
 
 def decode_message(bits):
     encode_length = 2
     byte_size = 8
-    item_count_per_char = byte_size//encode_length
-    bit_str = ''.join([str(b) for b in bits])
+    item_count_per_char = byte_size // encode_length
+    bit_str = ''.join([format(b, '02b') for b in bits])
     msg = ''
-    for i in range(0, encode_length, 8):
-        byte = bit_str[i: i + 8]
-        if len(byte) == 8:
-            print(int(byte))
-            msg += chr(int(byte))
+    for i in range(0, len(bit_str), byte_size):
+        byte = bit_str[i: i + byte_size]
+        if len(byte) == byte_size:
+            msg += chr(int(byte, 2))
     return msg
 
 
 def embed_message(img, msg):
     encoded_msg = encode_message(msg)
-    height, width = img.size
+    width, height = img.size
     pixels = img.load()
+    msg_len = len(encoded_msg)
+    if msg_len > width * height:
+        raise ValueError("Message is too long to encode in this image.")
+
+    index = 0
     for h in range(height):
-        is_msg_limit_reached = False
-        for w in range(width):
-            current_index = h * width + w
-            pixel = pixels[h, w]
-            pixel = (pixel & ~0b11) | encoded_msg[current_index]
-            pixels[h, w] = pixel
-            if current_index + 1 == len(encoded_msg):
-                is_msg_limit_reached = True
-                break
-        if is_msg_limit_reached:
+        if index >= msg_len:
             break
+        for w in range(width):
+            if index >= msg_len:
+                break
+            pixel = pixels[w, h]
+            pixel = (pixel & ~0b11) | encoded_msg[index]
+            pixels[w, h] = pixel
+            index += 1
     return img
 
 
 def extract_message(img):
     pixels = img.load()
-    height, width = img.size
+    width, height = img.size
     bits = []
 
     for h in range(height):
         for w in range(width):
-            pixel = pixels[h, w]
-            bits.append(pixel & ~0b11)
+            pixel = pixels[w, h]
+            bits.append(pixel & 0b11)
 
     return decode_message(bits)
 
@@ -70,7 +72,7 @@ def main():
 
     encoded_img = embed_message(img, msg)
 
-    # encoded_img.show()
+    encoded_img.save(PROJECT_DIR.joinpath('src/images/source', 'Lenna_encoded.png'))
 
     print(extract_message(encoded_img))
 
